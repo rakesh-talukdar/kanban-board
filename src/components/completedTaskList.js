868c7b6cee664fec50d8a201dd7dc56e95434a00
RAKESH TALUKDAR <rakesh.talukdar@capillarytech.com>
2020-10-08 16:46:16 +0530

    Refactored CRUD functionality for task card in completedTaskList component. Replaced class with react hooks

--- src/components/completedTaskList.js
+++ src/components/completedTaskList.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import UpdateTask from './updateTaskComponent';
 
-class CompletedTaskList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tasks: [],
-            task: '',
-            error: false,
-            errorText: '',
-        };
-    }
 
-    componentDidMount() {
-        this.getCompletedTaskList();
-    }
+const CompletedTaskList = () => {
+    const [tasks, setTasks] = useState([]);
+    const [task, setTask] = useState('');
+    const [error, setError] = useState({ hasError: false, errorMsg: '' });
+    const [taskAdded, setTaskAdded] = useState(false);
+    const [taskDeleted, setTaskDeleted] = useState(false);
+    const [showUpdateTaskForm, setUpdateFormVisibility] = useState(false);
+    const [taskId, setTaskId] = useState(false);
 
-    getCompletedTaskList = () => {
-        axios.get('http://localhost:3000/tasks')
-            .then((response) => response.data)
-            .then((data) => {
-                this.setState({
-                    tasks: data,
-                    task: '',
-                });
-            })
-            .catch((error) => console.error(error));
-    };
 
-    displayCompletedTaskList = () => {
-        if (this.state.tasks !== undefined) {
-            const completedTasks = this.state.tasks.map((task) => {
+    useEffect(() => {
+        const getCompletedTaskList = (async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/tasks');
+                const data = response.data;
+                setTasks(data);
+                setTask('');
+                setTaskDeleted(false);
+                setTaskAdded(false);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, [taskAdded, taskDeleted, showUpdateTaskForm]);
+
+
+    const displayCompletedTaskList = () => {
+        if (tasks !== undefined) {
+            const completedTasks = tasks.map((task) => {
                 if (task.status === 'completed') {
                     return (
                         <li key={task.id} className='task-card'>
-                            <span onClick={(event) => { this.toggleUpdateForm(event.target.id) }} id={task.id} className={'task-title'}>{task.task}</span>
-                            <button className='delete-task-btn' id={task.id} onClick={this.handleDelete}>X</button>
+                            <span onClick={() => { toggleUpdateTaskForm(task.id) }} className={'task-title'}>{task.task}</span>
+                            <button className='delete-task-btn' id={task.id} onClick={handleTaskDelete}>X</button>
                         </li>
                     );
                 }
@@ -47,98 +47,78 @@ class CompletedTaskList extends Component {
         }
     };
 
-    handleChange = (event) => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value,
-        });
-    }
+    const handleTaskNameChange = (event) => {
+        setTask(event.target.value);
+    };
 
-    handleSubmit = async (event) => {
+
+
+    const handleTaskSubmit = async (event) => {
         event.preventDefault();
         const data = {
-            task: this.state.task,
+            task,
             status: 'completed',
         };
 
-        if (data.task.length > 2) {
+        if (task.length > 2) {
             try {
                 const response = await axios({
                     method: 'post',
                     url: 'http://localhost:3000/tasks',
                     data,
                 });
-                if (response.status === 201 || response.status === 200) {
-                    this.getCompletedTaskList();
-                    this.setState({
-                        error: false,
-                    });
-                }
+                setTaskAdded(true);
+                setError({ hasError: false });
             } catch (error) {
-                this.setState({
-                    error: true,
-                    errorText: 'Oops!! Couldn\'t able to add',
-                });
+                setError({ hasError: true, errorMsg: 'Oops!! Couldn\'t able to add' });
             }
         } else {
-            this.setState({
-                error: true,
-                errorText: 'Task length should be minimum 2 characters!!',
-            });
+            setError({ hasError: true, errorMsg: 'Task length should be minimum 2 characters!!' });
         }
-    }
+    };
 
-    handleDelete = async (event) => {
+    const handleTaskDelete = async (event) => {
         event.preventDefault();
         const taskId = event.target.id;
         try {
             const response = await axios.delete(`http://localhost:3000/tasks/${taskId}`);
-            if (response.status === 200) {
-                this.getCompletedTaskList();
-            }
+            setTaskDeleted(true);
         } catch (error) {
-            console.error('Oops!! Couldn\'t able to delete', error);
+            console.error('Oops!! Couldn\'t able to delete');
         }
-    }
+    };
 
-    toggleUpdateForm = (taskId) => {
-        this.setState({
-            showUpdateForm: !this.state.showUpdateForm,
-            taskId,
-        })
-    }
 
-    render() {
-        return (
-            <div className='task-list-card'>
-                <header className='task-list-header'>
-                    <h3 >Completed</h3>
-                </header>
-                <ul className='task-list'>
-                    {this.state.showUpdateForm === true ?
-                        <UpdateTask
-                            taskId={this.state.taskId}
-                            todoList={this.getCompletedTaskList}
-                            toggle={this.toggleUpdateForm} />
-                        : null}
-                    {this.displayCompletedTaskList()}
-                </ul>
-                <p className='error-display'>{this.state.errorText}</p>
-                <form className='add-task-form' onSubmit={this.handleSubmit}>
-                    <input
-                        type='text'
-                        className='add-task-input'
-                        name='task'
-                        placeholder='Add task'
-                        onChange={this.handleChange}
-                        value={this.state.task}
+    const toggleUpdateTaskForm = (taskId) => {
+        setUpdateFormVisibility(!showUpdateTaskForm);
+        setTaskId(taskId);
+        setError({ hasError: false });
+    };
 
-                    />
-                    <button type='submit' className='add-task-btn'>Add</button>
-                </form>
-            </div>
-        );
-    }
-}
+
+    return (
+        < div className='task-list-card' >
+            <header className='task-list-header'>
+                <h3 >Completed</h3>
+            </header>
+            <ul className='task-list'>
+                {showUpdateTaskForm === true ? <UpdateTask taskId={taskId} toggle={toggleUpdateTaskForm} /> : null}
+                {displayCompletedTaskList()}
+            </ul>
+            <p className='error-display'>{error.hasError === true && error.errorMsg}</p>
+            <form className='add-task-form' onSubmit={handleTaskSubmit}>
+                <input
+                    type='text'
+                    className='add-task-input'
+                    name='task'
+                    placeholder='Add task'
+                    onChange={handleTaskNameChange}
+                    value={task}
+                />
+                <button type='submit' className='add-task-btn'>Add</button>
+            </form>
+        </div >
+    );
+};
 
 export default CompletedTaskList;
