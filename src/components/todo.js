0740c5e885879cc6f586e74fd60f191487aa58bb
RAKESH TALUKDAR <rakesh.talukdar@capillarytech.com>
2020-10-08 15:57:16 +0530

    Refactored functionalities of todo component by replacing class with react hooks.

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,43 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import UpdateTask from './updateTaskComponent';
 
-class Todo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            tasks: [],
-            task: '',
-            error: false,
-            errorText: '',
-            showUpdateForm: false,
-        };
-    }
+const Todo = (props) => {
+    const [tasks, setTasks] = useState([]);
+    const [task, setTask] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
+    const [taskAdded, setTaskAdded] = useState(false);
+    const [taskDeleted, setTaskDeleted] = useState(false);
+    const [showUpdateTaskForm, setUpdateFormVisibility] = useState(false);
+    const [taskId, setTaskId] = useState(false);
 
-    componentDidMount() {
-        this.getTodoList();
-    }
 
-    getTodoList = () => {
-        axios.get('http://localhost:3000/tasks')
-            .then((response) => response.data)
-            .then((data) => {
-                this.setState({
-                    tasks: data,
-                    task: '',
-                });
-            })
-            .catch((error) => console.error(error));
-    };
+    useEffect(() => {
+        const getTodoList = (async () => {
+            try {
+                const response = await axios.get('http://localhost:3000/tasks');
+                const data = response.data;
+                setTasks(data);
+                setTask('');
+                setTaskDeleted(false);
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, [taskAdded, taskDeleted, showUpdateTaskForm]);
 
-    displayTodoList = () => {
-        if (this.state.tasks !== undefined) {
-            const todoTasks = this.state.tasks.map((task) => {
+
+
+
+    const displayTodoList = () => {
+        if (tasks !== undefined) {
+            const todoTasks = tasks.map((task) => {
                 if (task.status === 'todo') {
                     return (
                         <li key={task.id} className='task-card'>
-                            <span onClick={(event) => { this.toggleUpdateForm(event.target.id) }} id={task.id} className={'task-title'}>{task.task}</span>
-                            <button className='delete-task-btn' id={task.id} onClick={this.handleDelete}>X</button>
+                            <span onClick={() => { toggleUpdateTaskForm(task.id) }} className={'task-title'}>{task.task}</span>
+                            <button className='delete-task-btn' id={task.id} onClick={handleTaskDelete}>X</button>
                         </li>
                     );
                 }
@@ -48,21 +47,21 @@ class Todo extends Component {
         }
     };
 
-    handleChange = (event) => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value,
-        });
-    }
 
-    handleSubmit = async (event) => {
+    const handleTaskNameChange = (event) => {
+        setTask(event.target.value);
+    };
+
+
+
+    const handleTaskSubmit = async (event) => {
         event.preventDefault();
         const data = {
-            task: this.state.task,
+            task,
             status: 'todo',
         };
 
-        if (data.task.length > 2) {
+        if (task.length > 2) {
             try {
                 const response = await axios({
                     method: 'post',
@@ -70,72 +69,59 @@ class Todo extends Component {
                     data,
                 });
                 if (response.status === 201 || response.status === 200) {
-                    this.getTodoList();
-                    this.setState({
-                        error: false,
-                    });
+                    setTaskAdded(true);
                 }
             } catch (error) {
-                this.setState({
-                    error: true,
-                    errorText: 'Oops!! Couldn\'t able to add',
-                });
+                setErrorMsg('Oops!! Couldn\'t able to add');
             }
         } else {
-            this.setState({
-                error: true,
-                errorText: 'Task length should be minimum 2 characters!!',
-            });
+            setErrorMsg('Task length should be minimum 2 characters!!');
         }
-    }
+    };
 
-    handleDelete = async (event) => {
+    const handleTaskDelete = async (event) => {
         event.preventDefault();
         const taskId = event.target.id;
         try {
             const response = await axios.delete(`http://localhost:3000/tasks/${taskId}`);
             if (response.status === 200) {
-                this.getTodoList();
+                setTaskDeleted(true);
             }
         } catch (error) {
             console.error('Oops!! Couldn\'t able to delete');
         }
-    }
+    };
 
 
-    toggleUpdateForm = (taskId) => {
-        this.setState({
-            showUpdateForm: !this.state.showUpdateForm,
-            taskId,
-        })
-    }
+    const toggleUpdateTaskForm = (taskId) => {
+        setUpdateFormVisibility(!showUpdateTaskForm);
+        setTaskId(taskId);
+    };
 
 
-    render() {
-        return (
-            <div className='task-list-card'>
-                <header className='task-list-header'>
-                    <h3 >Todo</h3>
-                </header>
-                <ul className='task-list'>
-                    {this.state.showUpdateForm === true ? <UpdateTask taskId={this.state.taskId} todoList={this.getTodoList} toggle={this.toggleUpdateForm} /> : null}
-                    {this.displayTodoList()}
-                </ul>
-                <p className='error-display'>{this.state.errorText}</p>
-                <form className='add-task-form' onSubmit={this.handleSubmit}>
-                    <input
-                        type='text'
-                        className='add-task-input'
-                        name='task'
-                        placeholder='Add task'
-                        onChange={this.handleChange}
-                        value={this.state.task}
-                    />
-                    <button type='submit' className='add-task-btn'>Add</button>
-                </form>
-            </div>
-        );
-    }
-}
+    return (
+        < div className='task-list-card' >
+            <header className='task-list-header'>
+                <h3 >Todo</h3>
+            </header>
+            <ul className='task-list'>
+                {showUpdateTaskForm === true ? <UpdateTask taskId={taskId} toggle={toggleUpdateTaskForm} /> : null}
+                {displayTodoList()}
+            </ul>
+            <p className='error-display'>{errorMsg}</p>
+            <form className='add-task-form' onSubmit={handleTaskSubmit}>
+                <input
+                    type='text'
+                    className='add-task-input'
+                    name='task'
+                    placeholder='Add task'
+                    onChange={handleTaskNameChange}
+                    value={task}
+                />
+                <button type='submit' className='add-task-btn'>Add</button>
+            </form>
+        </div >
+    );
+};
 
 export default Todo;
