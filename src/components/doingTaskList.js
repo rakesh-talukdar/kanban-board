7f1856e977b2b9cb9f22971d5fdca443ae37df99
RAKESH TALUKDAR <rakesh.talukdar@capillarytech.com>
2020-10-08 11:47:17 +0530

    Added doingTaskList component and implemented add task feature on that.

---
+++ src/components/doingTaskList.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class DoingTaskList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [],
+            task: '',
+            error: false,
+            errorText: '',
+        };
+    }
+
+    componentDidMount() {
+        this.getDoingTaskList();
+    }
+
+    getDoingTaskList = () => {
+        axios.get('http://localhost:3000/tasks')
+            .then((response) => response.data)
+            .then((data) => {
+                this.setState({
+                    tasks: data,
+                    task: '',
+                });
+            })
+            .catch((error) => console.error(error));
+    };
+
+    displayDoingTaskList = () => {
+        if (this.state.tasks !== undefined) {
+            const doingTasks = this.state.tasks.map((task) => {
+                if (task.status === 'doing') {
+                    return (<li key={task.id} className='task-card'>{task.task}</li>);
+                }
+            });
+            return doingTasks;
+        } else {
+            return null;
+        }
+    };
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value,
+        });
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = {
+            task: this.state.task,
+            status: 'doing',
+        };
+
+        if (data.task.length > 2) {
+            try {
+                const response = await axios({
+                    method: 'post',
+                    url: 'http://localhost:3000/tasks',
+                    data,
+                });
+                if (response.status === 201 || response.status === 200) {
+                    this.getDoingTaskList();
+                }
+            } catch (error) {
+                this.setState({
+                    error: true,
+                    errorText: 'Oops!! Couldn\'t able to add',
+                });
+            }
+        } else {
+            this.setState({
+                error: true,
+                errorText: 'Task length should be minimum 2 characters!!',
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className='task-list-card'>
+                <header className='task-list-header'>
+                    <h3 >Doing</h3>
+                </header>
+                <ul className='task-list'>
+                    {this.displayDoingTaskList()}
+                </ul>
+                <p className='error-display'>{this.state.errorText}</p>
+                <form className='add-task-form' onSubmit={this.handleSubmit}>
+                    <input
+                        type='text'
+                        className='add-task-input'
+                        name='task'
+                        placeholder='Add task'
+                        onChange={this.handleChange}
+                        value={this.state.task}
+                    />
+                    <button type='submit' className='add-task-btn'>Add</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default DoingTaskList;
--- src/components/taskListContainer.js
+++ src/components/taskListContainer.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Todo from './todo';
+import DoingTaskList from './doingTaskList';
 
 class TaskListContainer extends Component {
     render() {
         return (
             <div className='task-list-container'>
                 <Todo />
+                <DoingTaskList />
             </div>
         );
     }
